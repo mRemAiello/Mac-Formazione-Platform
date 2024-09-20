619c7bf54740f566709211e3b4bc6002fb16e70b
Mirko R. Aiello <aiello@doneref.com>
2024-09-20 17:27:50 +0200

    salto parte 2

--- Assets/Scripts/Camera/CameraFollow.cs
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -6,14 +6,29 @@ public class CameraFollow : MonoBehaviour
     public float smoothSpeed = 0.125f; // Velocità di transizione per il movimento della camera
     public Vector3 offset; // Distanza tra la camera e il personaggio
     public Vector3 negativeOffset;
-    private Vector3 desiredPosition = Vector3.zero;
+    private Vector3 desiredPosition;
+    private Rigidbody2D rb;
 
-    void Update()
+    void Start()
     {
+        rb = target.gameObject.GetComponent<Rigidbody2D>();
+        desiredPosition = (Vector3)rb.position + offset;
+    }
+
+    void LateUpdate()
+    {
+        // 
+        if (target == null || rb == null)
+        {
+            Debug.LogWarning("Assegna un Target o RB al tuo target.");
+            return;
+        }
+
+        //
         float moveInput = Input.GetAxisRaw("Horizontal");
 
         //
-        if (moveInput > 0)
+        /*if (moveInput > 0)
         {
             // Posizione desiderata della camera con offset
             desiredPosition = target.position + negativeOffset;
@@ -22,7 +37,10 @@ public class CameraFollow : MonoBehaviour
         {
             // Posizione desiderata della camera con offset
             desiredPosition = target.position + offset;
-        }
+        }*/
+
+        //
+        desiredPosition = (Vector3)rb.position + offset;
         
         // Movimento liscio della camera verso la posizione desiderata
         Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -4,6 +4,8 @@ public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;        // Velocità di movimento
     public float jumpForce = 10f;       // Forza del salto
+    public float doubleJumpForce = 5f;
+    public float simpleGravity = 0.2f;
     public LayerMask groundLayer;       // Layer che rappresenta il terreno
     public Transform groundCheck;       // Oggetto che verifica il contatto con il terreno
     public float groundCheckRadius = 0.2f; // Raggio per controllare se il personaggio è sul terreno
@@ -22,12 +24,15 @@ public class PlayerMovement : MonoBehaviour
     {
         // TODO: Inserire animazioni
 
-        // Input orizzontale per il movimento (GetAxisRaw prende SOLO 0, 1, -1)ù
+        // Input orizzontale per il movimento (GetAxisRaw prende SOLO 0, 1, -1)
         // GetAxis prende anche valori intermedi (es. 0.01, -0.01)
         float moveInput = Input.GetAxisRaw("Horizontal"); // Raccoglie l'input orizzontale (-1, 0, 1)
         
         // Movimento del rigidbody impostando la velocità
         rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
+
+        //
+        Debug.Log("Velocita 1: " + rb.velocity);
 
         // Flip della sprite in base alla direzione
         if (moveInput > 0)
@@ -47,6 +52,13 @@ public class PlayerMovement : MonoBehaviour
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce); // Aggiunge forza verticale per il salto
         }
+
+        // Gravità
+        rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y - simpleGravity);
+
+        Debug.Log("Velocita 2: " + rb.velocity);
+
+        // Limito la velocità x e y
     }
 
     void OnDrawGizmosSelected()
