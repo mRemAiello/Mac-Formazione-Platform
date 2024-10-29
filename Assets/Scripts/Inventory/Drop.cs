e574ab91dbfc0003ba04f97fd767dc1975cd2ca9
Mirko R. Aiello <aiello@doneref.com>
2024-10-29 12:03:06 +0100

    Inventario parte 2

--- Assets/Scripts/Inventory/Drop.cs
+++ Assets/Scripts/Inventory/Drop.cs
@@ -1,8 +1,17 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Drop : MonoBehaviour
 {
+    [Header("Animation")]
+    [SerializeField] private float _timeBetweenSpawn = 0.4f;
+    [SerializeField] private float _minForceX = 0;
+    [SerializeField] private float _maxForceX = 0;
+    [SerializeField] private float _minForceY = 0;
+    [SerializeField] private float _maxForceY = 0;
+
+    [Header("Drops")]
     [SerializeField] private int _itemsToDrop = 1;
     [SerializeField] private List<Droppable> _drops;
 
@@ -21,9 +30,7 @@ public class Drop : MonoBehaviour
             Debug.Log("Generato " + number);
             if (number <= droppable.Probability)
             {
-                GameObject obj = Instantiate(droppable.Data.Prefab, transform.position, Quaternion.identity);
-                Rigidbody2D rb = obj.GetComponent<Rigidbody2D>();
-                // TODO: Mettere una velocità x casuale, y casuale, spawnare ogni tot secondi
+                StartCoroutine(SpawnItem(droppable, itemDropped, itemDropped * _timeBetweenSpawn));
 
                 //
                 itemDropped++;
@@ -35,5 +42,31 @@ public class Drop : MonoBehaviour
                 break;
             }
         }
+    }
+
+    private IEnumerator SpawnItem(Droppable droppable, int itemIndex, float time)
+    {
+        yield return new WaitForSeconds(time);
+
+        //
+        GameObject obj = Instantiate(droppable.Data.Prefab, transform.position, Quaternion.identity);
+        
+        //
+        float x;
+        float y = Random.Range(_minForceY, _maxForceY);
+        if (itemIndex % 2 == 0)
+        {
+            x = -Random.Range(_minForceX, _maxForceX);
+        }
+        else
+        {
+            x = Random.Range(_minForceX, _maxForceX);
+        }
+
+        //
+        Rigidbody2D rb = obj.GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector2(x, y);
+
+        // TODO: Mettere una velocità x casuale, y casuale, spawnare ogni tot secondi
     }
 }
--- Assets/Scripts/Inventory/Item.cs
+++ Assets/Scripts/Inventory/Item.cs
@@ -1,21 +1,82 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+
+// TODO: Ease DotTween
+// https://i.redd.it/mutfwze2koz41.gif
 
 public class Item : MonoBehaviour
 {
     [SerializeField] private ItemData _itemData;
 
-    void OnCollisionEnter2D(Collision2D other)
+    [Space]
+    [SerializeField] private Transform _groundCheck;
+    [SerializeField] private LayerMask _groundLayer;
+    [SerializeField] private float _groundCheckRadius = 0.2f;
+    [SerializeField] private float _velocityThreshold = 0.1f;
+
+    [Space]
+    [SerializeField] private Rigidbody2D _rb;
+    [SerializeField] private Collider2D _collider;
+
+    //
+    private bool _isAnimationFinished = false;
+    private bool _isItemPicked = false;
+
+    void LateUpdate()
     {
+        if (_isAnimationFinished)
+        {
+            return;
+        }
+
+        if (IsGrounded())
+        {
+            _rb.velocity = Vector3.zero;
+            Destroy(_rb);
+            _collider.isTrigger = true;
+
+            //
+            _isAnimationFinished = true;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (_isItemPicked)
+        {
+            return;
+        }
+
+        Debug.Log("Chiamata");
+        Debug.Log(other.gameObject);
+        Debug.Log(other.gameObject.tag);
         if (other.gameObject.tag.Equals("Player"))
         {
             PlayerInventory.Instance.AddToInventory(_itemData);
-            
-            // TODO: Inserire un'animazione
-            // TODO: Disattivare il collider, lanciare un'animazione
-            Destroy(this);
+
+            //
+            _isItemPicked = true;
+
+            // TODO: lanciare Coroutine
+
+            // TODO: Fare animazione
+            Destroy(gameObject);
+
+            // TODO: Suono
         }
+    }
+
+    private bool IsGrounded()
+    {
+        // Controllo se il personaggio è a terra in base alla collisione e alla velocità verticale
+        bool groundedByCollision = Physics2D.OverlapCircle(_groundCheck.position, _groundCheckRadius, _groundLayer);
+
+        // Se è in contatto con il terreno e la velocità verticale è sufficientemente bassa, consideralo a terra
+        if (groundedByCollision && Mathf.Abs(_rb.velocity.y) <= _velocityThreshold)
+        {
+            return true;
+        }
+
+        return false;
     }
 
     // TODO: Animazione di "morte"
