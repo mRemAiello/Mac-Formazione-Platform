85fca03896656b702df9e7316519cff567b26140
Mirko R. Aiello <aiello@doneref.com>
2024-11-14 12:05:17 +0100

    Enemy second pass

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -1,11 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
     [SerializeField] private EnemyData _enemyData;
     [SerializeField] private Rigidbody2D _rb;
+    [SerializeField] private SpriteRenderer _spriteRenderer;
     [SerializeField] private Animator _animator;
 
     [Header("Patrol")]
@@ -14,48 +13,170 @@ public class Enemy : MonoBehaviour
 
     //
     private Transform _currentPoint;
+    private float _distanceToEnemy;
+    private bool _enemyInSight = false;
+    private bool _enemyInMeleeRange = false;
 
     void Start()
     {
-        _currentPoint = _pointA.transform;
+        _currentPoint = _pointB.transform;
     }
 
     void Update()
     {
-        // 
-        if (_enemyData.CanPatrol)
+        //
+        Patrol();
+
+        //
+        CheckEnemyInSight();
+
+        // TODO: Attacco melee / ranged
+        FollowEnemy();
+
+        // TODO: 
+        //MeleeAttack();
+
+        // TODO: 
+        //RangedAttack();
+
+        // TODO: Aggro Range superato, torna indietro e fai Patrolling
+
+        //
+        UpdateAnimator();
+    }
+
+    private void Flip()
+    {
+        if (_spriteRenderer.flipX)
         {
-            if (_currentPoint == _pointB.transform)
-            {
-                _rb.velocity = new Vector2(_enemyData.WalkSpeed, 0);
-            }
-            else
-            {
-                _rb.velocity = new Vector2(-_enemyData.WalkSpeed, 0);
-            }
-
-            //
-            if (Vector2.Distance(transform.position, _currentPoint.position) < 0.5f && _currentPoint == _pointB.transform)
-            {
-                _currentPoint = _pointA.transform;
-            }
-            if (Vector2.Distance(transform.position, _currentPoint.position) < 0.5f && _currentPoint == _pointA.transform)
-            {
-                _currentPoint = _pointB.transform;
-            }
-
-            //
-            _animator.SetFloat("XVelocity", _rb.velocity.x);
+            transform.localScale = new Vector3(1, 1, 1);
         }
+        else
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+    }
+
+    private void Patrol()
+    {
+        // 
+        if (!_enemyData.CanPatrol)
+            return;
+
+        //
+        if (_enemyInSight)
+            return;
+
+        //
+        if (_currentPoint == _pointB.transform)
+        {
+            _rb.velocity = new Vector2(_enemyData.WalkSpeed, 0);
+        }
+        else
+        {
+            _rb.velocity = new Vector2(-_enemyData.WalkSpeed, 0);
+        }
+
+        //
+        float distance = Vector2.Distance(transform.position, _currentPoint.position);
+        if (distance < _enemyData.PatrolWaypointThreshold && _currentPoint == _pointB.transform)
+        {
+            Turn();
+        }
+        else if (distance < _enemyData.PatrolWaypointThreshold && _currentPoint == _pointA.transform)
+        {
+            Turn();
+        }
+    }
+
+    private void Turn()
+    {
+        //
+        _rb.velocity = Vector2.zero;
+
+        //
+        float timeToLaunch = Random.Range(_enemyData.TurnAnimationDurationMin, _enemyData.TurnAnimationDurationMax);
+
+        //
+        Invoke(nameof(EndTurn), timeToLaunch);
+    }
+
+    private void EndTurn()
+    {
+        float distance = Vector2.Distance(transform.position, _currentPoint.position);
+        if (distance < _enemyData.PatrolWaypointThreshold && _currentPoint == _pointB.transform)
+        {
+            _currentPoint = _pointA.transform;
+            Flip();
+        }
+        else if (distance < _enemyData.PatrolWaypointThreshold && _currentPoint == _pointA.transform)
+        {
+            _currentPoint = _pointB.transform;
+            Flip();
+        }
+    }
+
+    // TODO: Implementare knockback
+    private void CheckEnemyInSight()
+    {
+        _distanceToEnemy = Vector2.Distance(transform.position, PlayerMovement.Instance.transform.position);
+        if (_distanceToEnemy < _enemyData.SightRadius)
+        {
+            _enemyInSight = true;
+        }
+        else
+        {
+            _enemyInSight = false;
+
+            // TODO: impostare punto A o punto B
+        }
+    }
+
+    void FollowEnemy()
+    {
+        // 
+        if (!_enemyInSight)
+            return;
+
+        // TODO: Nemico in range di attacco, return
+
+        //
+        if (transform.position.x > PlayerMovement.Instance.transform.position.x)
+        {
+            _rb.velocity = new Vector2(-_enemyData.RunSpeed, 0); 
+        }
+        else
+        {
+            _rb.velocity = new Vector2(_enemyData.RunSpeed, 0); 
+        }
+    }
+
+    private void UpdateAnimator()
+    {
+        //
+        _animator.SetFloat("XVelocity", Mathf.Abs(_rb.velocity.x));
+
+        //
+        _animator.SetBool("EnemyInSight", _enemyInSight);
     }
 
     void OnDrawGizmos()
     {
+        // Patrol
         if (_pointA != null)
             Gizmos.DrawWireSphere(_pointA.transform.position, 0.5f);
         if (_pointB != null)
             Gizmos.DrawWireSphere(_pointB.transform.position, 0.5f);
         if (_pointA != null && _pointB != null)
             Gizmos.DrawLine(_pointA.transform.position, _pointB.transform.position);
+
+        // Sight
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, _enemyData.SightRadius);
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, _enemyData.MeleeRange);
+
+        //
+        Gizmos.color = Color.white;
     }
 }
--- Assets/Scripts/Enemy/EnemyData.cs
+++ Assets/Scripts/Enemy/EnemyData.cs
@@ -5,10 +5,29 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Enemy/Enemy Data")]
 public class EnemyData : ScriptableObject
 {
+    [Header("Patrol")]
     [SerializeField] private bool _canPatrol = false;
+    [SerializeField] private float _patrolWaypointThreshold = 0.5f;
+    [SerializeField] private float _turnAnimationDurationMin = 0.3f;
+    [SerializeField] private float _turnAnimationDurationMax = 0.3f;
+
+    [Header("Melee Attack")]
+    [SerializeField] private bool _canMeleeAttack = false;
+    [SerializeField] private float _sightRadius = 4;
+    [SerializeField] private float _meleeRange = 2;
+
+    [Header("Movement")]
     [SerializeField] private float _walkSpeed = 2;
+    [SerializeField] private float _runSpeed = 4;
 
     //
     public bool CanPatrol => _canPatrol;
+    public float TurnAnimationDurationMin => _turnAnimationDurationMin;
+    public float TurnAnimationDurationMax => _turnAnimationDurationMax;
+    public float PatrolWaypointThreshold => _patrolWaypointThreshold;
+    public bool CanMeleeAttack => _canMeleeAttack;
+    public float SightRadius => _sightRadius;
+    public float MeleeRange => _meleeRange;
     public float WalkSpeed => _walkSpeed;
+    public float RunSpeed => _runSpeed;
 }
